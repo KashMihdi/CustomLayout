7237a75f61096d8eae12460fe8af545093ebbb14
KashMihdi <127855730+KashMihdi@users.noreply.github.com>
2023-10-26 16:57:14 +0300

    Initial Commit

---
+++ CustomLayout/CustomLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomLayoutApp.swift
+//  CustomLayout
+//
+//  Created by Kasharin Mikhail on 26.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct CustomLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
